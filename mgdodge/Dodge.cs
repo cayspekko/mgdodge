a9dd994ebaa210b7c1a3b8cbdca52995ba58f3b5
doug <doug@applegate.us>
2018-10-21 13:20:16 -0600

    Initial Commit

---
+++ mgdodge/Dodge.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System;
+
+namespace mgdodge
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Dodge : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        private List<GameObject> goes;
+        public GameObject player_one;
+
+        public Dodge()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+            player_one = new PlayerOne(this);
+            goes = new List<GameObject>
+            {
+                player_one,
+                new Ball(this, new Vector2(player_one.vector.X, 140))
+            };
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// game-specific content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+            foreach( var go in goes)
+            {
+                if (!go.dead)
+                    go.Update();
+            }
+            // check for collisions
+            foreach( var go in goes.ToArray())
+            {
+                foreach( var col in goes.ToArray())
+                {
+                    if (go == col) continue;
+                    if (col.BoundingBox.Intersects(go.BoundingBox)){
+                        go.Collision(col);
+                    }
+                }
+            }
+
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            spriteBatch.Begin();
+            foreach (var go in goes)
+            {
+                go.Draw(spriteBatch);
+            }
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        public void game_over()
+        {
+            foreach( var go in goes)
+            {
+                go.dead = true;
+            }
+            goes.Add(new GameObject(this, "game_over", new Vector2((GraphicsDevice.Viewport.Bounds.Width / 2)-32, (GraphicsDevice.Viewport.Bounds.Height / 2)-32)));
+        }
+    }
+}
---
+++ mgdodge/GameObject.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+
+namespace mgdodge
+{
+    public class GameObject
+    {
+        private Texture2D texture;
+        protected Dodge game;
+        public Vector2 vector;
+        public Boolean dead = false;
+
+        public Rectangle BoundingBox
+        {
+            get
+            {
+
+                return new Rectangle(
+                    (int) vector.X,
+                    (int) vector.Y,
+                    texture.Width,
+                    texture.Height);
+            }
+        }
+
+
+    public GameObject(Dodge game, string content, Vector2 starting_point)
+        {
+            this.game = game;
+            texture = game.Content.Load<Texture2D>(content);
+            vector = starting_point;
+        }
+
+        public virtual void Update()
+        {
+
+        }
+
+        public void Draw(SpriteBatch sb)
+        {
+            sb.Draw(texture, vector, Color.White);
+        }
+
+        public virtual void Collision(GameObject col)
+        {
+
+        }
+    }
+
+    public class PlayerOne: GameObject
+    {
+        private int dir = -1;
+        public PlayerOne(Dodge game) : base(game, "blue_square", new Vector2((game.GraphicsDevice.Viewport.Bounds.Width / 2) - 32, game.GraphicsDevice.Viewport.Bounds.Height - 64))
+        {
+            
+        }
+
+        public override void Update()
+        {
+            var mouseState = Mouse.GetState();
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                dir = -1;
+            }
+            if (mouseState.RightButton == ButtonState.Pressed)
+            {
+                dir = 1;
+            }
+            if (dir == -1 && vector.X <= 0)
+            {
+                dir = 0;
+                vector.X = 0;
+            }
+            if (dir == 1 && vector.X >= (game.GraphicsDevice.Viewport.Bounds.Width - 64))
+            {
+                dir = 0;
+                vector.X = game.GraphicsDevice.Viewport.Bounds.Width - 64;
+            }
+            vector.X = vector.X + (10 * dir);
+
+            base.Update();
+        }
+
+        public override void Collision(GameObject col)
+        {
+            game.game_over();
+            base.Collision(col);
+        }
+    }
+
+    public class Ball : GameObject
+    {
+        public Ball(Dodge game, Vector2 starting_point) : base(game, "red_circle", starting_point)
+        {
+
+        }
+
+        public override void Update()
+        {
+            if (vector.Y >= game.GraphicsDevice.Viewport.Bounds.Height)
+            {
+                vector.Y = -64;
+                vector.X = game.player_one.vector.X;
+            }
+            vector.Y = vector.Y + 10;
+
+            base.Update();
+        }
+    }
+}
